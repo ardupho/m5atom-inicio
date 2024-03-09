6e8daadba52a0952ebf76f9df5110a21e10710be
ardupho <ardupho@gmail.com>
2024-03-09 19:39:36 +0100

    mejoras en led-display

--- factory-test/src/main.cpp
+++ factory-test/src/main.cpp
@@ -3,22 +3,23 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN       27  
-#define NUMPIXELS 1   
+#define NUMPIXELS 25 
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(
     NUMPIXELS, PIN,
-    NEO_GRB + NEO_KHZ800);  // set number of LEDs, pin number, LED type.
+    NEO_GRB + NEO_KHZ800);  // establecer el número de LED, número de pin, tipo de LED.
                         
 uint32_t Wheel(byte WheelPos);
 void rainbow(uint8_t wait);
 
 void setup() {
-    M5.begin();      // Init M5Atom.
-    pixels.begin();  // Init the NeoPixel library.  
+    M5.begin();      // inicializa M5Atom.
+    pixels.begin();  // inicializa la libreria NeoPixel.
+    pixels.setBrightness(50); // Establezca BRILLO en aproximadamente 1/5 (máx. = 255)  
 }
 
 void loop() {
-    rainbow(20);
+    rainbow(25);
 }
 
 void rainbow(uint8_t wait) {
@@ -27,16 +28,15 @@ void rainbow(uint8_t wait) {
     for (j = 0; j < 256; j++) {
         for (i = 0; i < pixels.numPixels(); i++) {
             pixels.setPixelColor(i, Wheel((i + j) & 255));
-            while (M5.Btn.read() == 1)
-                ;
+            while (M5.Btn.read() == 1);
         }
         pixels.show();
         delay(wait);
     }
 }
 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
+// Ingrese un valor de 0 a 255 para obtener un valor de color.
+// Los colores son una transición r - g - b - de regreso a r..
 uint32_t Wheel(byte WheelPos) {
     WheelPos = 255 - WheelPos;
     if (WheelPos < 85) {
